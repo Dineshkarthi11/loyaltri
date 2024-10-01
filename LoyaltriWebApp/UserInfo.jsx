7d5562e33282adc7e7d67ea3f245b2dfc473ff59
Dineshkarthi11 <166858866+Dineshkarthi11@users.noreply.github.com>
2024-10-01 16:07:18 +0530

    UserInfo.jsx

--- LoyaltriWebApp/UserInfo.jsx
+++ LoyaltriWebApp/UserInfo.jsx
@@ -1,18 +1,17 @@
-
 import React from "react";
 
 function UserInfo() {
   return (
-    <div className="flex relative flex-col flex-1 items-start mt-4 text-sm font-medium text-white">
-      <h1 className="text-xl w-[150px] font-semibold">Loyaltri Web Application</h1>
-      <p className="self-stretch mt-3.5 text-zinc-100">
-        Valid till 06 Jan, 2025
-      </p>
-      <p className="mt-12 text-zinc-100">Users</p>
-      <p className="mt-2.5 text-base font-extrabold">
-        25/<span className="text-sm">50</span>{" "}
-        <span className="text-sm">active users</span>
-      </p>
+    <div className="text-sm font-medium text-white">
+
+      {/* User Info */}
+      <div className="mt-12">
+        <p className="text-zinc-100">Users</p>
+        <p className="mt-2 text-base font-extrabold">
+          <span className="text-600">25/</span>
+          <span className="text-sm">50</span> active users
+        </p>
+      </div>
     </div>
   );
 }
