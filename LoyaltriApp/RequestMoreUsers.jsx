6a21b09717d3a922f8c4a78b7b4c0b887c45d966
Dineshkarthi11 <166858866+Dineshkarthi11@users.noreply.github.com>
2024-10-01 16:05:14 +0530

    RequestMoreUsers.jsx

--- LoyaltriApp/RequestMoreUsers.jsx
+++ LoyaltriApp/RequestMoreUsers.jsx
@@ -10,23 +10,19 @@ function RequestMoreUsers() {
 
   return (
     <button
-      className="flex gap-2 items-center mt-36"
+      className="flex items-center gap-2 text-sm font-medium text-gray-500"
       aria-label="Request More Users"
-      onClick={handleNavigate} // Added this line
+      onClick={handleNavigate}
     >
-      <div className="flex gap-3.5 items-center self-stretch my-auto w-6">
-        <div className="flex gap-1 justify-center items-center self-stretch px-1 my-auto w-6 h-6 rounded-2xl bg-neutral-300 min-h-[24px]">
-          <img
-            loading="lazy"
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/ca096d1b98ba628a450f6b24f974c2a023ebce77af0be0e61c529557ea400de7?placeholderIfAbsent=true&apiKey=740fe41628444c68b4015f1a2abbfb39"
-            alt=""
-            className="object-contain self-stretch my-auto w-4 aspect-square"
-          />
-        </div>
+      <div className="w-6 h-6 bg-neutral-300 rounded-2xl flex items-center justify-center">
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/ca096d1b98ba628a450f6b24f974c2a023ebce77af0be0e61c529557ea400de7?placeholderIfAbsent=true&apiKey=740fe41628444c68b4015f1a2abbfb39"
+          alt=""
+          className="w-4"
+        />
       </div>
-      <span className="self-stretch my-auto text-sm font-medium tracking-tight text-gray-500">
-        Request More User
-      </span>
+      <span>Request More Users</span>
     </button>
   );
 }
