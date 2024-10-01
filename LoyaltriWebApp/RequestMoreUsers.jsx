23b4eedb143a295649a8851fc2ff6ea86068b5be
Dineshkarthi11 <166858866+Dineshkarthi11@users.noreply.github.com>
2024-10-01 16:07:01 +0530

    RequestMoreUsers.jsx

--- LoyaltriWebApp/RequestMoreUsers.jsx
+++ LoyaltriWebApp/RequestMoreUsers.jsx
@@ -1,23 +1,29 @@
-
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function RequestMoreUsers() {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate("/request-additional-user");
+  };
+
   return (
-    <div className="flex gap-2 items-center mt-36">
-      <div className="flex gap-3.5 items-center self-stretch my-auto w-6">
-        <div className="flex gap-1 justify-center items-center self-stretch px-1 my-auto w-6 h-6 bg-violet-400 rounded-2xl min-h-[24px]">
-          <img
-            loading="lazy"
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/ca096d1b98ba628a450f6b24f974c2a023ebce77af0be0e61c529557ea400de7?placeholderIfAbsent=true&apiKey=740fe41628444c68b4015f1a2abbfb39"
-            alt=""
-            className="object-contain self-stretch my-auto w-4 aspect-square"
-          />
-        </div>
+    <button
+      className="flex items-center gap-2 text-sm font-medium text-gray-500"
+      aria-label="Request More Users"
+      onClick={handleNavigate}
+    >
+      <div className="w-6 h-6 bg-neutral-300 rounded-2xl flex items-center justify-center">
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/ca096d1b98ba628a450f6b24f974c2a023ebce77af0be0e61c529557ea400de7?placeholderIfAbsent=true&apiKey=740fe41628444c68b4015f1a2abbfb39"
+          alt=""
+          className="w-4"
+        />
       </div>
-      <button className="self-stretch my-auto text-sm font-medium tracking-tight text-white">
-        Request More User
-      </button>
-    </div>
+      <span>Request More Users</span>
+    </button>
   );
 }
 
