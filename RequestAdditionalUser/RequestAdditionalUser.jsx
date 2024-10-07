4a363656133c0209d210a0fe8d63f86e147a738e
Dineshkarthi11 <166858866+Dineshkarthi11@users.noreply.github.com>
2024-10-07 15:23:45 +0530

    RequestAdditionalUser.jsx

--- RequestAdditionalUser/RequestAdditionalUser.jsx
+++ RequestAdditionalUser/RequestAdditionalUser.jsx
@@ -3,33 +3,53 @@ import Header from "./Header";
 import UserCountInput from "./UserCountInput";
 import CouponCodeInput from "./CouponCodeInput";
 import ActionButtons from "./ActionButtons";
+import ModalAnt from "../../../common/ModalAnt";
+import HeaderThird from "../RequestAdditionalUserMore/HeaderThird";
+import UserCountInputOne from "../RequestAdditionalUserMore/UserCountInputOne";
+import DiscountCode from "../RequestAdditionalUserMore/DiscountCode";
+import TotalAmount from "../RequestAdditionalUserMore/TotalAmount";
 
 function RequestAdditionalUser() {
+   const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <div className="relative h-screen w-full flex items-center justify-center backdrop-blur-md bg-white/30">
-      {/* Full-page background with blur */}
-      <div className="absolute inset-0 backdrop-blur-md bg-white/30 z-0"></div>
-
       {/* Card container */}
       <main className="relative z-10 flex flex-col justify-center py-1.5 bg-gradient-to-b from-blue-100 to-white to-[20%] rounded-3xl border border-solid border-zinc-100 max-w-[447px] shadow-[0px_31px_60px_rgba(59,55,75,0.1)]">
         <section className="flex overflow-hidden relative flex-col items-center py-5 w-full rounded-2xl max-w-[437px] shadow-[0px_10px_15px_rgba(182,181,254,0.19)]">
           <Header />
           <UserCountInput />
-          <CouponCodeInput />
+          <CouponCodeInput onsubmit={()=>{
+            setIsModalOpen(true)
+          }} 
+           />
           <ActionButtons />
-          <button
-            className="flex overflow-hidden absolute z-0 justify-center items-center self-start px-2 w-8 h-8 bg-white rounded-2xl border border-solid border-black border-opacity-10 right-[17px] top-[19px]"
-            aria-label="Close"
-          >
-            <img
-              loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/8b828ea14f3e06e30832d2ce6a8360ec188de026c4a0c1761f948c52421a41ab?placeholderIfAbsent=true&apiKey=740fe41628444c68b4015f1a2abbfb39"
-              alt=""
-              className="object-contain self-stretch my-auto aspect-square w-[18px]"
-            />
-          </button>
         </section>
       </main>
+
+      <ModalAnt
+        isVisible={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        // width="435px"
+        showOkButton={true}
+        cancelText="Request"
+        okText="Make Payment"
+        okButtonClass="mx-[15px] w-[190px]"
+        cancelButtonClass="w-[190px]"
+        showCancelButton={true}
+        showTitle={false}
+        centered={true}
+        padding="8px"
+        customButton={false}
+      >
+        <section className="flex overflow-hidden relative flex-col items-center py-5 w-[437px] h-[320px] rounded-2xl max-w-[437px]">
+          <HeaderThird />
+          <UserCountInputOne />
+          <DiscountCode />
+          <TotalAmount />
+        </section>
+      </ModalAnt>
+     
     </div>
   );
 }
