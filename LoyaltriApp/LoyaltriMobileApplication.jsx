bd2795dfa7fbef9d0591f0e8f3894b36063b9c8c
Dineshkarthi11 <166858866+Dineshkarthi11@users.noreply.github.com>
2024-10-07 15:20:55 +0530

    LoyaltriMobileApplication.jsx

--- LoyaltriApp/LoyaltriMobileApplication.jsx
+++ LoyaltriApp/LoyaltriMobileApplication.jsx
@@ -3,10 +3,10 @@ import ExpiryNotification from "./ExpiryNotification";
 import UserCount from "./UserCount";
 import RequestMoreUsers from "./RequestMoreUsers";
 import ModalAnt from "../../../common/ModalAnt";
-import Header from "../RequestAdditionalUser/Header";
 import UserCountInput from "../RequestAdditionalUser/UserCountInput";
 import CouponCodeInput from "../RequestAdditionalUser/CouponCodeInput";
 import ActionButtons from "../RequestAdditionalUser/ActionButtons";
+import HeaderSecond from "../RequestAdditionalUser/HeaderSecond";
 
 function LoyaltriMobileApplication() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -23,11 +23,16 @@ function LoyaltriMobileApplication() {
         />
 
         {/* Top-left: Title and Validity */}
-        <div className="absolute top-4 left-5 z-10">
+        {/* <header className="absolute top-4 left-5 z-10">
           <h1 className="text-xl w-[150px] font-semibold text-violet-600">
-            Loyaltri Web Application
+            Loyaltri Mobile Application
           </h1>
           <p className="mt-1 text-gray-500">Valid till 06 Jan, 2025</p>
+        </header> */}
+        
+        <div className="absolute top-4 left-5 z-10">
+            <h1 className="text-xl w-[150px] font-semibold text-violet-600">Loyaltri Web Application</h1>
+           <p className="mt-1 text-gray-500">Valid till 06 Jan, 2025</p>
         </div>
 
         {/* Top-right: Expiry Notification */}
@@ -42,29 +47,28 @@ function LoyaltriMobileApplication() {
 
         {/* Bottom-right: Request More Users */}
         <div className="absolute bottom-4 right-5 z-10">
-          <RequestMoreUsers
-            onsubmit={() => {
-              setIsModalOpen(true);
-            }}
-          />
+          <RequestMoreUsers onsubmit={()=>{
+            setIsModalOpen(true)
+          }} />
         </div>
       </div>
-
-      {/* Modal for additional user request */}
       <ModalAnt
         isVisible={isModalOpen}
         onClose={() => setIsModalOpen(false)}
-        showOkButton={false} // Hide the default Ok button since we are using custom buttons
-        showCancelButton={false} // Hide the default Cancel button
-        showTitle={false} // Remove the title as we use custom content
+        // width="435px"
+        showOkButton={true}
+        cancelText="Request"
+        okText="Make Payment"
+        okButtonClass="mx-[15px] w-[190px]"
+        cancelButtonClass="w-[190px]"
+        showCancelButton={true}
+        showTitle={false}
         centered={true}
         padding="8px"
-        customButton={
-          <ActionButtons /> // Using ActionButtons component for Request and Make Payment buttons
-        }
+        customButton={false}
       >
-        <section className="flex overflow-hidden relative flex-col items-center py-5 w-[437px] h-[310px] rounded-2xl max-w-[437px]">
-          <Header />
+        <section className="flex overflow-hidden relative flex-col items-center py-5 w-[437px] h-[320px] rounded-2xl max-w-[437px]">
+          <HeaderSecond />
           <UserCountInput />
           <CouponCodeInput />
         </section>
