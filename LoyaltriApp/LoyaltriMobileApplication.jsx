994d103a0e087fabedfa0e141d8fdbec2047e126
Dineshkarthi11 <166858866+Dineshkarthi11@users.noreply.github.com>
2024-10-07 15:57:22 +0530

    LoyaltriMobileApplication.jsx

--- LoyaltriApp/LoyaltriMobileApplication.jsx
+++ LoyaltriApp/LoyaltriMobileApplication.jsx
@@ -3,24 +3,13 @@ import ExpiryNotification from "./ExpiryNotification";
 import UserCount from "./UserCount";
 import RequestMoreUsers from "./RequestMoreUsers";
 import ModalAnt from "../../../common/ModalAnt";
-import RequestAdditionalUser from "../RequestAdditionalUser/RequestAdditionalUser";
-import RequestAdditionalUserMore from "../RequestAdditionalUserMore/RequestAdditionalUserMore";
+import UserCountInput from "../RequestAdditionalUser/UserCountInput";
+import CouponCodeInput from "../RequestAdditionalUser/CouponCodeInput";
+import ActionButtons from "../RequestAdditionalUser/ActionButtons";
+import HeaderSecond from "../RequestAdditionalUser/HeaderSecond";
 
 function LoyaltriMobileApplication() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [showAdditionalUser, setShowAdditionalUser] = useState(false);
-  const [showAdditionalUserMore, setShowAdditionalUserMore] = useState(false);
-
-  // Handle the Apply button click in RequestAdditionalUser
-  const handleApplyClick = () => {
-    setShowAdditionalUserMore(true);
-    setShowAdditionalUser(false);
-  };
-
-  // Handle the Request More Users button click
-  const handleRequestMoreUsersClick = () => {
-    setShowAdditionalUser(true);
-  };
 
   return (
     <article className="relative flex flex-col w-[380px] h-[250px] min-w-[320px] min-h-[250px] bg-white rounded-2xl border-violet-600 border-opacity-20 overflow-hidden">
@@ -34,9 +23,16 @@ function LoyaltriMobileApplication() {
         />
 
         {/* Top-left: Title and Validity */}
-        <div className="absolute top-4 left-5 z-10">
-          <h1 className="text-xl w-[150px] font-semibold text-violet-600">Loyaltri Web Application</h1>
+        {/* <header className="absolute top-4 left-5 z-10">
+          <h1 className="text-xl w-[150px] font-semibold text-violet-600">
+            Loyaltri Mobile Application
+          </h1>
           <p className="mt-1 text-gray-500">Valid till 06 Jan, 2025</p>
+        </header> */}
+        
+        <div className="absolute top-4 left-5 z-10">
+            <h1 className="text-xl w-[150px] font-semibold text-violet-600">Loyaltri Web Application</h1>
+           <p className="mt-1 text-gray-500">Valid till 06 Jan, 2025</p>
         </div>
 
         {/* Top-right: Expiry Notification */}
@@ -51,28 +47,31 @@ function LoyaltriMobileApplication() {
 
         {/* Bottom-right: Request More Users */}
         <div className="absolute bottom-4 right-5 z-10">
-          <RequestMoreUsers onsubmit={handleRequestMoreUsersClick} />
+          <RequestMoreUsers onsubmit={()=>{
+            setIsModalOpen(true)
+          }} />
         </div>
       </div>
-
-      {/* Modal to show RequestAdditionalUser or RequestAdditionalUserMore */}
       <ModalAnt
         isVisible={isModalOpen}
         onClose={() => setIsModalOpen(false)}
-        showOkButton={false}
+        // width="435px"
+        showOkButton={true}
+        cancelText="Request"
+        okText="Make Payment"
+        okButtonClass="mx-[15px] w-[190px]"
         cancelButtonClass="w-[190px]"
-        showCancelButton={false}
+        showCancelButton={true}
         showTitle={false}
         centered={true}
         padding="8px"
+        customButton={false}
       >
-        {showAdditionalUser && (
-          <RequestAdditionalUser
-            onApply={handleApplyClick} // Pass function to handle "Apply" button
-          />
-        )}
-
-        {showAdditionalUserMore && <RequestAdditionalUserMore />}
+        <section className="flex overflow-hidden relative flex-col items-center py-5 w-[437px] h-[320px] rounded-2xl max-w-[437px]">
+          <HeaderSecond />
+          <UserCountInput />
+          <CouponCodeInput />
+        </section>
       </ModalAnt>
     </article>
   );
