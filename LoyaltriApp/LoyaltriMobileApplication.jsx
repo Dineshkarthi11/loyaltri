b76728ec0938863eb42d87525482c8726a1f06ef
Dineshkarthi11 <166858866+Dineshkarthi11@users.noreply.github.com>
2024-10-07 11:37:22 +0530

    LoyaltriMobileApplication.jsx

--- LoyaltriApp/LoyaltriMobileApplication.jsx
+++ LoyaltriApp/LoyaltriMobileApplication.jsx
@@ -1,30 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpiryNotification from "./ExpiryNotification";
 import UserCount from "./UserCount";
 import RequestMoreUsers from "./RequestMoreUsers";
+import ModalAnt from "../../../common/ModalAnt";
+import Header from "../RequestAdditionalUser/Header";
+import UserCountInput from "../RequestAdditionalUser/UserCountInput";
+import CouponCodeInput from "../RequestAdditionalUser/CouponCodeInput";
+import ActionButtons from "../RequestAdditionalUser/ActionButtons";
 
 function LoyaltriMobileApplication() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <article className="relative flex flex-col  h-full min-w-[320px] min-h-[250px] bg-white rounded-2xl border border-dashed border-violet-600 border-opacity-20 overflow-hidden">
+    <article className="relative flex flex-col w-[380px] h-[250px] min-w-[320px] min-h-[250px] bg-white rounded-2xl border-violet-600 border-opacity-20 overflow-hidden">
       <div className="relative flex flex-col h-full w-full p-5">
         {/* Background Image */}
         <img
           loading="lazy"
-          src="https://cdn.builder.io/api/v1/image/assets/TEMP/f725d06ded8b0b366880068fe2cc8e29b03c09aee3a3002cb232fa5ca9ab9354?placeholderIfAbsent=true&apiKey=740fe41628444c68b4015f1a2abbfb39"
+          src="https://github.com/Dineshkarthi11/loyaltri/blob/main/assets/Frame%201321316357.png?raw=true"
           alt=""
           className="absolute inset-0 w-full h-full object-cover z-0"
         />
 
         {/* Top-left: Title and Validity */}
-        <header className="absolute top-4 left-5 z-10">
+        <div className="absolute top-4 left-5 z-10">
           <h1 className="text-xl w-[150px] font-semibold text-violet-600">
-            Loyaltri Mobile Application
+            Loyaltri Web Application
           </h1>
           <p className="mt-1 text-gray-500">Valid till 06 Jan, 2025</p>
-        </header>
+        </div>
 
         {/* Top-right: Expiry Notification */}
-        <div className="absolute top-4 right-5 z-10">
+        <div className="absolute top-4 right-3 z-10">
           <ExpiryNotification />
         </div>
 
@@ -35,9 +42,33 @@ function LoyaltriMobileApplication() {
 
         {/* Bottom-right: Request More Users */}
         <div className="absolute bottom-4 right-5 z-10">
-          <RequestMoreUsers />
+          <RequestMoreUsers
+            onsubmit={() => {
+              setIsModalOpen(true);
+            }}
+          />
         </div>
       </div>
+
+      {/* Modal for additional user request */}
+      <ModalAnt
+        isVisible={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        showOkButton={false} // Hide the default Ok button since we are using custom buttons
+        showCancelButton={false} // Hide the default Cancel button
+        showTitle={false} // Remove the title as we use custom content
+        centered={true}
+        padding="8px"
+        customButton={
+          <ActionButtons /> // Using ActionButtons component for Request and Make Payment buttons
+        }
+      >
+        <section className="flex overflow-hidden relative flex-col items-center py-5 w-[437px] h-[310px] rounded-2xl max-w-[437px]">
+          <Header />
+          <UserCountInput />
+          <CouponCodeInput />
+        </section>
+      </ModalAnt>
     </article>
   );
 }
