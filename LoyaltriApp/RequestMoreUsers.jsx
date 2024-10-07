54f76dba49e82194254740ea9e975cb5ecc8812d
Dineshkarthi11 <166858866+Dineshkarthi11@users.noreply.github.com>
2024-10-07 15:22:05 +0530

    RequestMoreUsers.jsx

--- LoyaltriApp/RequestMoreUsers.jsx
+++ LoyaltriApp/RequestMoreUsers.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-function RequestMoreUsers() {
+function RequestMoreUsers({onsubmit= ()=>{}}) {
   const navigate = useNavigate();
 
   const handleNavigate = () => {
@@ -12,7 +12,7 @@ function RequestMoreUsers() {
     <button
       className="flex items-center gap-2 text-sm font-medium text-gray-500"
       aria-label="Request More Users"
-      onClick={handleNavigate}
+      onClick={onsubmit}
     >
       <div className="w-6 h-6 bg-neutral-300 rounded-2xl flex items-center justify-center">
         <img
