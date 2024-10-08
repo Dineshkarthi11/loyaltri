8a4890eb52c0d116e974c5d1332b78ec823a1425
Dineshkarthi11 <166858866+Dineshkarthi11@users.noreply.github.com>
2024-10-08 14:24:43 +0530

    LoyaltriMobileApplication.jsx

--- LoyaltriApp/LoyaltriMobileApplication.jsx
+++ LoyaltriApp/LoyaltriMobileApplication.jsx
@@ -5,17 +5,14 @@ import RequestMoreUsers from "./RequestMoreUsers";
 import ModalAnt from "../../../common/ModalAnt";
 import UserCountInput from "../RequestAdditionalUser/UserCountInput";
 import CouponCodeInput from "../RequestAdditionalUser/CouponCodeInput";
+import ActionButtons from "../RequestAdditionalUser/ActionButtons";
 import HeaderSecond from "../RequestAdditionalUser/HeaderSecond";
-import RequestAdditionalUserMore from "./RequestAdditionalUserMore";
+import HeaderActive from "../ActiveInactiveUsers/HeaderActive";
+import UserSearch from "../ActiveInactiveUsers/UserSearch";
+import UserList from "../ActiveInactiveUsers/UserList";
 
 function LoyaltriMobileApplication() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [showAdditionalUserMore, setShowAdditionalUserMore] = useState(false); // State to handle navigation
-
-  // Function to trigger navigation to RequestAdditionalUserMore
-  const handleApplyClick = () => {
-    setShowAdditionalUserMore(true);
-  };
 
   return (
     <article className="relative flex flex-col w-[380px] h-[250px] min-w-[320px] min-h-[250px] bg-white rounded-2xl border-violet-600 border-opacity-20 overflow-hidden">
@@ -29,11 +26,16 @@ function LoyaltriMobileApplication() {
         />
 
         {/* Top-left: Title and Validity */}
-        <div className="absolute top-4 left-5 z-10">
+        {/* <header className="absolute top-4 left-5 z-10">
           <h1 className="text-xl w-[150px] font-semibold text-violet-600">
-            Loyaltri Web Application
+            Loyaltri Mobile Application
           </h1>
           <p className="mt-1 text-gray-500">Valid till 06 Jan, 2025</p>
+        </header> */}
+        
+        <div className="absolute top-4 left-5 z-10">
+            <h1 className="text-xl w-[150px] font-semibold text-violet-600">Loyaltri Web Application</h1>
+           <p className="mt-1 text-gray-500">Valid till 06 Jan, 2025</p>
         </div>
 
         {/* Top-right: Expiry Notification */}
@@ -48,39 +50,33 @@ function LoyaltriMobileApplication() {
 
         {/* Bottom-right: Request More Users */}
         <div className="absolute bottom-4 right-5 z-10">
-          <RequestMoreUsers
-            onsubmit={() => {
-              setIsModalOpen(true);
-            }}
-          />
+          <RequestMoreUsers onsubmit={()=>{
+            setIsModalOpen(true)
+          }} />
         </div>
       </div>
+      <ModalAnt
+        isVisible={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        // width="435px"
+        showOkButton={true}
+        cancelText="Request"
+        okText="Make Payment"
+        okButtonClass="mx-[15px] w-[190px]"
+        cancelButtonClass="w-[190px]"
+        showCancelButton={true}
+        showTitle={false}
+        centered={true}
+        padding="8px"
+        customButton={false}
+      >
+        <section className="flex overflow-hidden relative flex-col items-center py-5 w-[437px] h-[320px] rounded-2xl max-w-[437px]">
+          <HeaderSecond />
+          <UserCountInput />
+          <CouponCodeInput />
+        </section>
 
-      {/* Conditional rendering for different modals */}
-      {showAdditionalUserMore ? (
-        <RequestAdditionalUserMore />
-      ) : (
-        <ModalAnt
-          isVisible={isModalOpen}
-          onClose={() => setIsModalOpen(false)}
-          showOkButton={true}
-          cancelText="Request"
-          okText="Make Payment"
-          okButtonClass="mx-[15px] w-[190px]"
-          cancelButtonClass="w-[190px]"
-          showCancelButton={true}
-          showTitle={false}
-          centered={true}
-          padding="8px"
-          customButton={false}
-        >
-          <section className="flex overflow-hidden relative flex-col items-center py-5 w-[437px] h-[320px] rounded-2xl max-w-[437px]">
-            <HeaderSecond />
-            <UserCountInput />
-            <CouponCodeInput onApply={handleApplyClick} /> {/* Pass onApply handler */}
-          </section>
-        </ModalAnt>
-      )}
+      </ModalAnt>
     </article>
   );
 }
