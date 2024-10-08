ac2b9f0ed657ea4e863b2ec661843746f17c2d11
Dineshkarthi11 <166858866+Dineshkarthi11@users.noreply.github.com>
2024-10-08 14:35:33 +0530

    UserCount.jsx

--- LoyaltriApp/UserCount.jsx
+++ LoyaltriApp/UserCount.jsx
@@ -1,19 +1,12 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
-function UserCount({onsubmit= ()=>{}}) {
-  const navigate = useNavigate();
-
-  const handleUserCountClick = () => {
-    navigate("/active-inactive-users");
-  };
-
+function UserCount({ onsubmit = () => {} }) {
   return (
     <div>
       <p className="text-gray-500">Users</p>
       <p
         className="text-base font-extrabold cursor-pointer mt-1"
-        onClick={onsubmit}
+        onClick={onsubmit} // Trigger onsubmit on click
       >
         <span className="text-violet-600">25/</span>
         <span className="text-sm text-violet-600">50</span>{" "}
